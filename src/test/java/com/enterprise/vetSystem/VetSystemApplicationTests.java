795f78c9980725f497bac62ace733b032d05fb13
Gabriella Franco <gabriella-nena@hotmail.com>
2024-12-29 19:19:40 -0300

    Fixed dependencies

---
+++ src/test/java/com/enterprise/vetSystem/VetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.vetSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
